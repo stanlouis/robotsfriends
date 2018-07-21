98b7dae6ee07f12de3c5268e6ad3ab0fb3807c8a
Stanley Louis <stanlouis@msn.com>
2018-07-21 08:16:11 -0400

    Simulate loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
+import ReactLoading from 'react-loading';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
 
-import { robots } from './robots';
 import './app.css';
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchField: ''
     };
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      fetch('http://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => this.setState({ robots: users }));
+    }, 2000);
+  }
   onSearchChange = event => {
     this.setState({ searchField: event.target.value });
   };
@@ -21,13 +28,23 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter(robot =>
       robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
     );
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+
+    if (this.state.robots.length === 0) {
+      return (
+        <div className="center">
+         <h1 className="tc">Loading</h1> 
+          <ReactLoading type="cylon" color="444" height={'20%'} width={'20%'} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
