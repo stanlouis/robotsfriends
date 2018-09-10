302c054d30b6327468ce7b70b9fa749f3b697fa1
Stanley Louis <stanlouis@msn.com>
2018-09-10 16:08:16 -0400

    Replace state.searchfield with redux

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,31 +1,41 @@
-import React, { Component } from "react";
-import ReactLoading from "react-loading";
-import axios from "axios";
-import CardList from "../components/CardList";
-import SearchBox from "../components/SearchBox";
-import Scroll from "../components/Scroll";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import ReactLoading from 'react-loading';
+import axios from 'axios';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import './app.css';
 
-import "./app.css";
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+  return {
+    searchField: state.searchField
+  };
+};
+
+const mapDispatchToProps = dispatch => ({
+  onSearchChange: e => dispatch(setSearchField(e.target.value))
+});
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: [],
-      searchField: ""
+      robots: []
     };
   }
 
   componentDidMount() {
     axios
-      .get("https://jsonplaceholder.typicode.com/users")
+      .get('https://jsonplaceholder.typicode.com/users')
       .then(users => this.setState({ robots: users.data }));
   }
-  onSearchChange = event => {
-    this.setState({ searchField: event.target.value });
-  };
 
   render() {
-    const { robots, searchField } = this.state;
+    const { robots } = this.state;
+    const { searchField, onSearchChange } = this.props;
     const filteredRobots = robots.filter(robot =>
       robot.name.toLowerCase().includes(searchField.toLowerCase())
     );
@@ -33,12 +43,12 @@ class App extends Component {
     return !robots.length ? (
       <div className="center">
         <h1 className="tc">Loading</h1>
-        <ReactLoading type="cylon" color="444" height={"20%"} width={"20%"} />
+        <ReactLoading type="cylon" color="444" height={'20%'} width={'20%'} />
       </div>
     ) : (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
+        <SearchBox searchChange={onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
         </Scroll>
@@ -47,4 +57,7 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
